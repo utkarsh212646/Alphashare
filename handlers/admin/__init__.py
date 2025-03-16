4c935c681f756390c200dd9ce0553ef092e9ccde
UTKARSHBOTS <90956407+utkarsh212646@users.noreply.github.com>
2025-03-16 09:39:12 +0530

    Add files via upload

---
+++ handlers/admin/__init__.py
@@ -0,0 +1,11 @@
+from .auto_delete import auto_delete_command
+from .broadcast import broadcast_command
+from .stats import stats_command
+from .upload import upload_command
+
+__all__ = [
+    'auto_delete_command',
+    'broadcast_command',
+    'stats_command',
+    'upload_command'
+]
